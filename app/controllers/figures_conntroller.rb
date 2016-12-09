34a56104631aa430bac56cf7c5d4f5892bfa9015
Ali Alrawi <ali.developer90@gmail.com>
2016-12-09 12:45:38 +0300

    checkpoint_2

--- app/controllers/figures_conntroller.rb
+++ app/controllers/figures_conntroller.rb
@@ -1,11 +1,26 @@
 class FiguresController < ApplicationController
+  get "/figures" do
+    @figures=Figure.all
+    erb :'/figures/index'
+  end
   get "/figures/new" do
    erb :'/figures/new'
   end
+  get "/figures/:id" do
+    @figure=Figure.find(params[:id])
+    erb :'/figures/show'
+  end
   post "/figures" do
-    @figure=Figure.create(params[:figure][:name])
-    @figure.title_ids << params[:figure][:title_ids][:id]
+    @figure=Figure.create(params[:figure])
+    if params[:title][:name] != ""
+       @figure.titles << Title.create(params[:title])
+    end
+
+    if params[:landmark][:name] != ""
+       @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
     @figure.save
-   erb :'/figures/show'
+    redirect to '/figures'
   end
 end
