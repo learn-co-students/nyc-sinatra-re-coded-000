82aeec029dc2de1aa3eb9c82373aa69ed4a37674
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-12-26 13:58:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
   get '/' do
-    erb :root
+    erb :'application/root'
   end
 end
